6e672cc430bf0a23bcfd951ad4a8ba8499755155
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-06-11 13:26:44 -0300

    se agrego alta local, modifico local

---
+++ src/pages/LocalAgregar.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar';
+
+import UsuarioForm from '../components/Usuario/UsuarioForm';
+import CabeceraListadoUsuarios from '../components/Usuario/CabeceraListadoUsuarios';
+import LineaUsuario from '../components/Usuario/LineaUsuario';
+import Locales_bd from '../bbdd/locales.json';
+import Usuarios_bd from '../bbdd/usuarios.json';
+import CabeceraUsuarios from '../components/Usuario/CabeceraUsuarios';
+import { useUser } from '../context/UsuarioContext';
+import { useNavigate } from 'react-router-dom';
+import LocalForm from '../components/Local/LocalForm';
+import ItemLocal from '../components/Local/ItemLocal';
+
+function LocalAgregar() {
+    const { usuarioCntxt } = useUser();
+    const [esNuevo,setEsNuevo] = useState(true)
+  const [esUsuarioLocal, setEsUsuarioLocal] = useState(false);
+  const navigate = useNavigate();
+  const [locales, setLocales] = useState(Locales_bd.locales);
+  const [usuarios, setUsuarios] = useState(Usuarios_bd.usuarios)
+  const [titulo, setTitulo] = useState('Agregar Local');
+  const [editar, setEditar] = useState(false);
+  const [usuarioEditar,setUsuarioEditar] = useState({})
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [dni, setDni] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [localesAsignados, setLocalesAsignados] = useState([]);
+
+  // Función para manejar el cambio de valores en los campos del formulario
+  const handleInputChange = (event, setValue) => {
+    setValue(event.target.value);
+  };
+
+  // Función para cargar datos desde un JSON
+  const cargarUsuario = (uname) => {
+    const usuarioEjemplo = usuarios.find(usuario => usuario.nombreUsuario === uname.nombreUsuario)
+    setEditar(true)
+    setTitulo('Editar Usuario');
+    setUsuarioEditar(usuarioEjemplo);
+    // Actualizamos el estado con los datos del JSON
+    setNombre(usuarioEjemplo.nombre);
+    setApellido(usuarioEjemplo.apellido);
+    setDni(usuarioEjemplo.dni);
+    setEmail(usuarioEjemplo.email);
+    setTelefono(usuarioEjemplo.telefono);
+  };
+
+
+  return (
+    <>
+    <Navbar/>
+    <div className='w-full h-screen bg-negro-2 flex flex-col'>
+        <div className='flex flex-col w-full h-5/12'>
+          <div className='p-4 flex items-center content-center '>
+            <h2 className='mx-5 text-2xl text-white font-bolder underline decoration-2 underline-offset-8'>{titulo}</h2>{/*cambia segun el usuario si se crea o se carga */ }
+          </div>
+          <LocalForm locales={locales} usuarios={usuarios}
+            nombre={nombre}
+            setNombre={setNombre}
+            apellido={apellido}
+            setApellido={setApellido}
+            dni={dni}
+            setDni={setDni}
+            email={email}
+            setEmail={setEmail}
+            telefono={telefono}
+            setTelefono={setTelefono}
+            localesAsignados={localesAsignados}
+            setLocalesAsignados={setLocalesAsignados}
+            handleInputChange={handleInputChange}
+            usuarioEditar={usuarioEditar}
+            editar={editar}
+          />
+        </div>
+        <div className=' flex-1 flex overflow-y-auto p-2 flex-wrap h-screen justify-center'>
+          {locales.map(l => {
+              return <ItemLocal local={l} esNuevo={esNuevo } />
+          })}
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default LocalAgregar
--- src/pages/Locales.js
+++ src/pages/Locales.js
@@ -1,8 +1,9 @@
 import React, {  useEffect, useState } from 'react'
 import ListadoLocales from '../components/Local/ListadoLocales'
 import Navbar from '../components/Navbar'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useUser } from '../context/UsuarioContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Locales({isAuthenticated}) {
   const {usuarioCntxt} = useUser()
@@ -18,7 +19,13 @@ function Locales({isAuthenticated}) {
     <Navbar/>
       <div className='w-full h-screen bg-negro-2'>
         <ListadoLocales locales={locales} />
-    </div>
+      <div className='w-full flex justify-center'>
+      <button className='w-1/3 bg-color-1 text-white h-20 rounded-full hover:bg-blanco hover:text-negro-1'>
+           <Link to={'/local'}><label className='font-semibold mx-5'>Agregar Local</label><FontAwesomeIcon icon="fa-solid fa-plus" /> </Link>
+            </button>
+        </div>
+      </div>
+        
     </>
   )
 }
