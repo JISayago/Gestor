1c46e3f75c235bec9578cf31fd2e9aeb280c486a
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-06-11 15:22:37 -0300

    redirecciones inicio-local y cambios en json de usuarios prop logeado

--- src/components/Local/ItemLocal.jsx
+++ src/components/Local/ItemLocal.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import LogoLocal from '../../assets/LogoLocal.png'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useUser } from '../../context/UsuarioContext';
 
 function ItemLocal({ local,esNuevo }) {
   const navigate = useNavigate();
+  const { setLogeado } = useUser()
+  
+  const handleClick = (e) => {
+    e.preventDefault();
+    setLogeado(local.id)
+    navigate(`/local/${local.id}`)
+  }
+  
   return (
     
     esNuevo ?
@@ -23,23 +32,24 @@ function ItemLocal({ local,esNuevo }) {
           </div>
         </div>
       </div>
-      
-    
     :
+      <div className='flex w-full h-3/5 m-10 p-2 items-center border-2 border-color-1 rounded-lg bg-blanco md:w-1/4'>
+        <div className='w-1/2 h-full'>
+          <img className='rounded-md' src={LogoLocal} />
+        </div>
+        <div className='flex flex-col w-1/2 h-full justify-around'>
+          <div className='w-full flex justify-center  p-3 '>
+            <label className='text-xl'>{local.nombre}</label> 
+          </div>
+          <div className='w-full flex justify-center  p-3 '>
+            <button onClick={(e) => handleClick(e)} className='flex flex-row justify-evenly items-center w-2/3 p-2 rounded-md bg-color-1 text-white mb-3 hover:bg-white hover:text-color-1'>
+              <label>Ingresar</label>
+              <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+            </button>
+          </div>
+        </div>
+      </div>
     
-    <div className='flex w-full h-3/5 m-10 p-2 items-center border-2 border-color-1 rounded-lg bg-blanco md:w-1/4'>
-    <div className='w-1/2 h-full'>
-    <img className='rounded-md' src={ LogoLocal} />
-    </div>
-    <div className='flex flex-col w-1/2 h-full justify-around'>
-    <div className='w-full flex justify-center  p-3 '>
-    <label className='text-xl'>{local.nombre}</label> 
-    </div>
-    <div className='w-full flex justify-center  p-3 '>
-    <button onClick={() => navigate(`/local/${local.id}`) } className='flex flex-row justify-evenly items-center w-2/3 p-2 rounded-md bg-color-1 text-white mb-3 hover:bg-white hover:text-color-1'><label>Ingresar</label><FontAwesomeIcon icon="fa-solid fa-arrow-right" /></button>
-    </div>
-    </div>
-    </div>
     
   )
 }
