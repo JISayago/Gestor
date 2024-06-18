ac6e46a62db5c57e4f1d9006aaebb5e3edbb7846
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2024-06-18 18:36:35 -0300

    0.

--- src/components/Venta/CabeceraListadoVentas.jsx
+++ src/components/Venta/CabeceraListadoVentas.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function CabeceraListadoVentas() {
   return (
     <div className='bg-color-1 w-full flex justify-between border border-negro-2 '>
-    <div className='border border-negro-2 h-full py-2 px-5 text-center w-1/12'><label className='font-bold text-white'>N° Venta</label></div>
+    <div className='border border-negro-2 h-full py-2 px-5 text-center w-1/12'><label className='font-bold text-white overflow-hidden text-ellipsis whitespace-nowrap'>N° Venta</label></div>
     <div className='border border-negro-2 h-full py-2 px-5 text-center w-2/12'><label className='font-bold text-white'>Local</label></div>
     <div className='border border-negro-2 h-full py-2 px-5 text-center w-2/12'><label className='font-bold text-white'>Fecha</label></div>
     <div className='border border-negro-2 h-full py-2 px-5 text-center w-2/12'><label className='font-bold text-white'>Vendedor</label></div>
